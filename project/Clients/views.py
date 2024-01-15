97f321c0ceadbaf61f0b08979c7388e725500dac
Matias Altamirano <m.altamirano30@outlook.com>
2024-01-15 09:53:21 -0300

    Actualice, urls, views, model y templates en clients

--- project/Clients/views.py
+++ project/Clients/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    clientes = Cliente.object.all()
+    return render(request, "Clients/index.html", {"Clientes": clientes})
--- project/config/settings.py
+++ project/config/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a%7e0n+q9_+mr4i))ps7uix_#2@8sfn1(kjs1+j^zzzsvsp7*a'
+from django.core.management.utils import get_random_secret_key
+
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
