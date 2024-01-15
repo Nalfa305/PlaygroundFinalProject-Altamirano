9401f757f59cbc210bdcae68fd92d11ef88220b9
Matias Altamirano <m.altamirano30@outlook.com>
2024-01-15 10:24:59 -0300

    Cambie views

--- project/Clients/views.py
+++ project/Clients/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from . import forms, models
+
 
 def index(request):
-    clientes = Cliente.object.all()
-    return render(request, "Clients/index.html", {"Clientes": clientes})
+    return render(request, "Clients/index.html")
+
+
+def pais_list(request):
+    paises = models.Pais.objects.all()
+    context = {"paises": paises}
+    return render(request, "Clients/pais_list.html", context)
+
+
+def cliente_list(request):
+    clientes = models.Cliente.objects.all()
+    context = {"clientes": clientes}
+    return render(request, "Clients/cliente_list.html", context)
+
+
+def cliente_create(request):
+    if request.method == "POST":
+        form = forms.ClienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("Clients:cliente_list")
+    else:  # if request.method == "GET":
+        form = forms.ClienteForm()
+    return render(request, "Clients/cliente_create.html", {"form": form})
